ea233f4886f5af225e02c3bc623f50ec63047fda
Nitzan <nitzanel2@gmail.com>
2016-08-22 19:24:36 +0300

    Created tests for the website

---
+++ web_tests.py
@@ -0,0 +1,73 @@
+import os
+from website import FlaskApp
+import unittest
+from flask import url_for
+
+
+class BaseTestCase(unittest.TestCase):
+	""" A base test case."""
+
+	def create_app(self):
+		flaskApp = FlaskApp()
+		flaskApp.app.config['TESTING'] =True
+		flaskApp.app.config['DEBUG'] =True
+		
+		return flaskApp
+
+	def setUp(self):
+	
+		self.client = self.create_app().app.test_client(self)
+
+	def tearDown(self):
+		pass
+
+class FlaskTestCase(BaseTestCase):
+	def test_about(self):
+		response = self.client.get('/about',content_type='html/text')
+		self.assertEqual(response.status_code,200)
+
+	def test_pan_immune(self):
+		response = self.client.get('/genes/pan_immune', content_type='html/text')
+		self.assertEqual(response.status_code,200)
+
+	def test_cell_type_specific(self):
+		response = self.client.get('/genes/cell_type_specific', content_type='html/text')
+		self.assertEqual(response.status_code,200)
+
+class GraphTestCase(BaseTestCase):
+	# Ensure a post request in pan_immune works for a known gene symbol
+	def test_pan_immune_post_exist(self):
+		response = self.client.post('/genes/pan_immune', data={'gene_name':'SON'})
+		self.assertEqual(response.status_code,200)
+		self.assertIn('there are graphs',response.data)
+
+	# Ensure a post request in pan_immune doesnt work for ''
+	def test_pan_immune_post_empty(self):
+		response = self.client.post('/genes/pan_immune', data={'gene_name':''})
+		self.assertEqual(response.status_code,200)
+		self.assertNotIn('there are graphs',response.data)
+
+	# Ensure a post request in pan_immune doesnt work for ' '
+	def test_pan_immune_post_space(self):
+		response = self.client.post('/genes/pan_immune', data={'gene_name':' '})
+		self.assertEqual(response.status_code,200)
+		self.assertNotIn('there are graphs',response.data)
+
+	# Ensure a post request in pan_immune doesnt work for something that is not a known gene in the database
+	def test_pan_immune_unknown_gene(self):
+		response = self.client.post('/genes/pan_immune', data={'gene_name':'notagene'})
+		self.assertEqual(response.status_code,200)
+		self.assertNotIn('there are graphs',response.data)	
+
+	# this test suppose to fail right now
+	def test_cell_type_specific_post(self):
+		#response = self.client.post('/genes/cell_type_specific', data={'gene_name':'SON'})
+		#self.assertEqual(response.status_code,400)
+		pass
+	def test_cell_type_specific_many(self):
+		pass
+
+
+
+if __name__ == '__main__':
+	unittest.main()
